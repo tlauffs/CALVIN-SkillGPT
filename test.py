59c3a0c9396a983b2bd5daf19e874d74959dbc1c
tlauffs <timlauffs@gmail.com>
2023-07-30 19:07:09 +0200

    first commit / Looked at hulc2 play data / visuilized data, embeddings and annotations

---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+test = [['place', 'red', 'circle', 'diagonal', 'yellow', 'hexagon'], ['blue', 'cube', 'right', 'of', 'red', 'circle'] ,['blue', 'cube', 'diagonally', 'yellow', 'heart']]
+
+npar = np.array(test)
+
+print(npar)
